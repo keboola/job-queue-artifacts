2953697a251dbd7ccd8befae5f9939d7d9a20998
Erik Zigo <erik.zigo@keboola.com>
2022-06-28 16:19:05 +0200

    ArtifactsTest - added more tests for uploadCurrent

--- tests/ArtifactsTest.php
+++ tests/ArtifactsTest.php
@@ -7,17 +7,44 @@ namespace Keboola\Artifacts\Tests;
 use DateTime;
 use Generator;
 use Keboola\Artifacts\Artifacts;
+use Keboola\Artifacts\ArtifactsException;
 use Keboola\Artifacts\Filesystem;
 use Keboola\StorageApi\Client as StorageClient;
+use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Options\ListFilesOptions;
 use Keboola\Temp\Temp;
+use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\Test\TestLogger;
+use RangeException;
 use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Throwable;
 
 class ArtifactsTest extends TestCase
 {
+    public function testGetFilesystem(): void
+    {
+        $temp = new Temp();
+
+        $artifacts = new Artifacts(
+            self::createMock(StorageClient::class),
+            self::createMock(Logger::class),
+            $temp,
+            'main-branch',
+            'keboola.orchestrator',
+            '123456',
+            '123456789',
+        );
+
+        self::assertSame(
+            $temp->getTmpFolder() . '/tmp',
+            $artifacts->getFilesystem()->getTmpDir()
+        );
+    }
+
     public function testUploadCurrent(): void
     {
         $temp = new Temp();
@@ -46,7 +73,7 @@ class ArtifactsTest extends TestCase
                 ->setQuery(sprintf('tags:jobId-%d*', $jobId))
         )[0];
 
-        self::assertEquals($fileId, $storageFile['id']);
+        self::assertSame($fileId, $storageFile['id']);
         self::assertContains('artifact', $storageFile['tags']);
         self::assertContains('branchId-branch-123', $storageFile['tags']);
         self::assertContains('componentId-keboola.component', $storageFile['tags']);
@@ -58,8 +85,81 @@ class ArtifactsTest extends TestCase
 
         $file1 = file_get_contents('/tmp/file1');
         $file2 = file_get_contents('/tmp/folder/file2');
-        self::assertEquals('{"foo":"bar"}', $file1);
-        self::assertEquals('{"foo":"baz"}', $file2);
+        self::assertSame('{"foo":"bar"}', $file1);
+        self::assertSame('{"foo":"baz"}', $file2);
+    }
+
+    public function uploadCurrentExceptionsHandlingData(): iterable
+    {
+        yield 'ProcessFailedException convert' => [
+            new ProcessFailedException(self::createMock(Process::class)),
+            ArtifactsException::class,
+            'Error uploading file: The command "" failed.',
+        ];
+        yield 'ClientException convert' => [
+            new ClientException('You don\'t have access to the resource.'),
+            ArtifactsException::class,
+            'Error uploading file: You don\'t have access to the resource.',
+        ];
+        yield 'random expection do not convert' => [
+            new RangeException('Test'),
+            RangeException::class,
+            'Test',
+        ];
+    }
+
+    /**
+     * @dataProvider uploadCurrentExceptionsHandlingData
+     * @param class-string<Throwable> $expectedException
+     */
+    public function testUploadCurrentExceptionsHandling(
+        Throwable $exception,
+        string $expectedException,
+        string $expectedExceptionMessage
+    ): void {
+        $storageClientMock = self::createMock(StorageClient::class);
+        $storageClientMock->expects(self::once())
+            ->method('uploadFile')
+            ->willThrowException($exception)
+        ;
+
+        $artifacts = new Artifacts(
+            $storageClientMock,
+            self::createMock(Logger::class),
+            new Temp(),
+            'main-branch',
+            'keboola.orchestrator',
+            '123456',
+            '123456789',
+        );
+
+        touch($artifacts->getFilesystem()->getCurrentDir() . '/test.txt');
+
+        $this->expectException($expectedException);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        $artifacts->uploadCurrent();
+    }
+
+    public function testUploadCurrentDoNotUploadIfNoFileExists(): void
+    {
+        $storageClientMock = self::createMock(StorageClient::class);
+        $storageClientMock
+            ->expects($this->never())
+            ->method($this->anything())
+        ;
+
+        $artifacts = new Artifacts(
+            $storageClientMock,
+            self::createMock(Logger::class),
+            new Temp(),
+            'main-branch',
+            'keboola.orchestrator',
+            '123456',
+            '123456789',
+        );
+
+        self::assertNull($artifacts->uploadCurrent());
     }
 
     public function testDownloadLatestRuns(): void
